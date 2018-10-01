0c762ad85ac036dfb295930102599be2e4665255
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-10-01 15:38:54 -0700

    memory match Oleksandr Dukun

---
+++ main.js
@@ -0,0 +1,102 @@
+$(document).ready(initializeApp);
+
+var firstCardClicked = null;
+var secondCardClicked = null;
+var frontOfTheCard = [
+    './images/chandlerbing1.jpg',
+    './images/girls.jpg',
+    './images/gunther1.jpg',
+    './images/joeytribbiani1.jpg',
+    './images/joye chand1.jpg',
+    './images/monicagaller1.jpg',
+    './images/phoebebuffet1.jpg',
+    './images/rachelgreen1.jpg',
+    './images/rossgaller1.jpg',
+    './images/chandlerbing1.jpg',
+    './images/girls.jpg',
+    './images/gunther1.jpg',
+    './images/joeytribbiani1.jpg',
+    './images/joye chand1.jpg',
+    './images/monicagaller1.jpg',
+    './images/phoebebuffet1.jpg',
+    './images/rachelgreen1.jpg',
+    './images/rossgaller1.jpg'
+];
+
+$('.front').css('background-color', 'black');
+
+//functions hve to be executed on load or reload.
+function initializeApp(){
+    createCardsOnTableGame();
+    //applyBackgroundImageForFrontOfTheCard();
+    applyEventHandlersToDom();
+
+}
+function makeRandomCardFromArray(){
+    frontOfTheCard.sort(function(a, b){return 0.5 - Math.random()});
+}
+function createCardsOnTableGame () {
+
+
+    makeRandomCardFromArray();
+    for (var cardIndex=0; cardIndex <frontOfTheCard.length; cardIndex++ ) {
+       var cardContainer = $('<div>').addClass('cardContainer');
+       var card = $('<div>').addClass('card');
+       var front = $('<div>').addClass('front');
+       var frontImage = $('<img>', {
+           class: 'imgSize',
+           src: frontOfTheCard[cardIndex]
+       });
+       //applyBackgroundImageForFrontOfTheCard(front,frontOfTheCard[cardIndex] )
+       var back = $('<div>').addClass('back');
+       front.append(frontImage);
+       card.append(front);
+       card.append(back);
+       cardContainer.append(card);
+       $('.gameTable').append(cardContainer);
+    }
+}
+
+function applyBackgroundImageForFrontOfTheCard(cardFront, image){
+
+    cardFront.css('background-image', 'url(' +image+')');
+}
+
+
+function applyEventHandlersToDom() {
+    $('.card').click(handleCardClick);
+}
+
+function handleCardClick() {
+    if ($(event.currentTarget).hasClass('hide')) {
+        return;
+    }
+
+    if (firstCardClicked === null){
+        firstCardClicked = event.currentTarget;
+        hideCard(event.currentTarget);
+    } else {
+        hideCard(event.currentTarget);
+        secondCardClicked = event.currentTarget;
+
+        if($(firstCardClicked).find('.front img').attr('src') === $(secondCardClicked).find('.front img').attr('src')) {
+            firstCardClicked = null;
+            secondCardClicked = null;
+
+        } else {
+            setTimeout(delayResetNotMatchedCards, 1000);
+        }
+    }
+
+}
+function delayResetNotMatchedCards() {
+    $(firstCardClicked).removeClass('hide');
+    $(secondCardClicked).removeClass('hide');
+    firstCardClicked = null;
+    secondCardClicked = null;
+}
+
+function hideCard(card) {
+
+    $(card).addClass('hide');
+}
