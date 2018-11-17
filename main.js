a1ad6b66cfd0aa989bd365a28f0dbc7b16f371c7
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-11-17 14:46:03 -0800

    completed stats

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ $(document).ready(initializeApp);
 var firstCardClicked = null;
 var secondCardClicked = null;
 var restrictCardClick = false;
+var countAttempts = 0;
+var countAccuracy = 0;
+var countMatches = 0;
+var gamesPlayed = 0;
+var countGames = 0;
 var frontOfTheCard = [
     './images/chandlerbing1.jpg',
     './images/girls.jpg',
@@ -30,11 +35,14 @@ var frontOfTheCard = [
 function initializeApp(){
     createCardsOnTableGame();
     applyEventHandlersToDom();
+    resetGame();
+    
 }
 
 function makeRandomCardFromArray(){
     frontOfTheCard.sort(function(a, b){return 0.5 - Math.random()});
 }
+
 function createCardsOnTableGame () {
 
     makeRandomCardFromArray();
@@ -55,10 +63,49 @@ function createCardsOnTableGame () {
     }
 }
 
+function resetGame(){
+    $('.reset').click(handleResetGame);
+    
+}
+function handleResetGame(){
+    firstCardClicked = null;
+    secondCardClicked = null;
+    restrictCardClick = false;
+    countAttempts = 0;
+    countMatches = 0;
+    countAccuracy = 0;
+    frontOfTheCard = [
+    './images/chandlerbing1.jpg',
+    './images/girls.jpg',
+    './images/gunther1.jpg',
+    './images/joeytribbiani1.jpg',
+    './images/joye chand1.jpg',
+    './images/monicagaller1.jpg',
+    './images/phoebebuffet1.jpg',
+    './images/rachelgreen1.jpg',
+    './images/rossgaller1.jpg',
+    './images/chandlerbing1.jpg',
+    './images/girls.jpg',
+    './images/gunther1.jpg',
+    './images/joeytribbiani1.jpg',
+    './images/joye chand1.jpg',
+    './images/monicagaller1.jpg',
+    './images/phoebebuffet1.jpg',
+    './images/rachelgreen1.jpg',
+    './images/rossgaller1.jpg'
+];
+    
+    $('.gameTable').empty();
+    createCardsOnTableGame ();
+    applyEventHandlersToDom();
+    $('.atemptsNumber').text(countAttempts);
+    countAccuracy = $('.accuracyNumber').text(countAccuracy)
+}
 
 function applyEventHandlersToDom() {
     $('.card').click(handleCardClick);
 }
+
 
 function handleCardClick() {
     if(restrictCardClick){
@@ -74,16 +121,33 @@ function handleCardClick() {
     } else {
         hideCard(event.currentTarget);
         secondCardClicked = event.currentTarget;
+        countAttempts++
+        
 
         if($(firstCardClicked).find('.front img').attr('src') === $(secondCardClicked).find('.front img').attr('src')) {
             firstCardClicked = null;
             secondCardClicked = null;
+            countMatches++;
+            
+            
 
         } else {
             restrictCardClick = true;
             setTimeout(delayResetNotMatchedCards, 1000);
         }
     }
+    $('.atemptsNumber').text(countAttempts);
+    if(countMatches>0){
+        countAccuracy = $('.accuracyNumber').text(((countMatches / countAttempts)*100).toFixed(0) + '%');
+    }
+    if(countMatches==9){
+        countGames++
+        gamesPlayed = $('.gameNumber').text(countGames);
+        $('.gameTable').empty();
+        setTimeout(handleResetGame, 2000);
+        
+    }
+    
 
 }
 function delayResetNotMatchedCards() {
